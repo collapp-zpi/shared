0e50b2e9d5e5ea8f8741202612861b51d4e36e8a
Karol Sitarz <ka.sitarz@gmail.com>
2021-10-28 21:59:49 +0200

    minor style and submit button updates

--- components/button/SubmitButton.tsx
+++ components/button/SubmitButton.tsx
@@ -1,19 +1,16 @@
 import { useApiRequest } from '../form/Form'
 import Button, { ButtonProps } from './Button'
 import { CgSpinner } from 'react-icons/cg'
-import { useFormContext } from 'react-hook-form'
 
 const SubmitButton = ({
   children = 'Submit',
   type = 'submit',
   ...props
 }: ButtonProps) => {
-  const { formState } = useFormContext()
-  const { isDirty } = formState
   const { isLoading } = useApiRequest()
 
   return (
-    <Button {...props} type={type} disabled={isLoading || !isDirty}>
+    <Button {...props} type={type} disabled={isLoading}>
       {isLoading && <CgSpinner className="animate-spin mr-2 -ml-2" />}
       {children}
     </Button>
