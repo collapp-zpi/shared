f1888ffb15b41708a979fbb916302f8d68b3d6a7
Karol Sitarz <ka.sitarz@gmail.com>
2023-06-25 15:50:50 +0200

    update emails

--- emailTemplates/invite.tsx
+++ emailTemplates/invite.tsx
@@ -9,6 +9,7 @@ import {
   Section,
   Tailwind,
 } from '@react-email/components'
+import { Font } from '@react-email/font'
 
 export const InviteTemplate = ({
   from,
@@ -20,17 +21,38 @@ export const InviteTemplate = ({
   redirect: string
 }) => (
   <Html>
-    <Head />
-    <Preview>You got invited in Collapp</Preview>
+    <Head>
+      <Font
+        fontFamily="Poppins"
+        fallbackFontFamily="Arial"
+        webFont={{
+          url: 'https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLGT9Z1xlFd2JQEk.woff2',
+          format: 'woff2',
+        }}
+        fontWeight={500}
+        fontStyle="normal"
+      />
+      <Font
+        fontFamily="Poppins"
+        fallbackFontFamily="Arial"
+        webFont={{
+          url: 'https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2',
+          format: 'woff2',
+        }}
+        fontWeight={700}
+        fontStyle="bold"
+      />
+    </Head>
+    <Preview>New invitation in Collapp 👀</Preview>
     <Tailwind>
       <Body className="bg-gray-200 p-8 text-gray-600 font-sans">
         <Container className="bg-gray-50 max-w-3xl mx-auto p-8 rounded-3xl text-center">
           <Section>
             <Img
-              src="https://collapp.sitarz.dev/logo.svg"
-              className="h-12 w-12 mx-auto opacity-25 mb-4"
+              src="https://collapp.live/logo.png"
+              className="h-8 w-8 mx-auto opacity-20 mb-6"
             />
-            <h1 className="text-4xl font-bold text-blue-500 mb-12">
+            <h1 className="text-4xl font-bold text-blue-500 mb-6">
               Hello, there! 👋
             </h1>
             <div className="text-lg mb-4">
@@ -42,7 +64,7 @@ export const InviteTemplate = ({
               className="px-6 py-3 bg-blue-500 mx-auto rounded-xl font-bold text-white cursor-pointer hover:bg-blue-600 shadow-xl transition-colors"
               target="_blank"
             >
-              Accept
+              See the invite
             </Button>
           </Section>
 
@@ -52,7 +74,7 @@ export const InviteTemplate = ({
           </div>
           <div className="text-gray-400 text-sm">
             Your friends at{' '}
-            <a href="">
+            <a href="https://collapp.live">
               <strong>Collapp</strong>
             </a>
           </div>
--- emailTemplates/sign-in.tsx
+++ emailTemplates/sign-in.tsx
@@ -9,6 +9,7 @@ import {
   Section,
   Tailwind,
 } from '@react-email/components'
+import { Font } from '@react-email/font'
 
 export const SignInTemplate = ({
   email,
@@ -18,17 +19,38 @@ export const SignInTemplate = ({
   redirect: string
 }) => (
   <Html>
-    <Head />
+    <Head>
+      <Font
+        fontFamily="Poppins"
+        fallbackFontFamily="Arial"
+        webFont={{
+          url: 'https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLGT9Z1xlFd2JQEk.woff2',
+          format: 'woff2',
+        }}
+        fontWeight={500}
+        fontStyle="normal"
+      />
+      <Font
+        fontFamily="Poppins"
+        fallbackFontFamily="Arial"
+        webFont={{
+          url: 'https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLCz7Z1xlFd2JQEk.woff2',
+          format: 'woff2',
+        }}
+        fontWeight={700}
+        fontStyle="bold"
+      />
+    </Head>
     <Preview>Sign in to Collapp</Preview>
     <Tailwind>
       <Body className="bg-gray-200 p-8 text-gray-600 font-sans">
-        <Container className="bg-gray-50 max-w-3xl mx-auto p-8 rounded-3xl text-center">
+        <Container className="bg-gray-50 max-w-2xl mx-auto p-8 rounded-3xl text-center">
           <Section>
             <Img
-              src="https://collapp.sitarz.dev/logo.svg"
-              className="h-12 w-12 mx-auto opacity-25 mb-4"
+              src="https://collapp.live/logo.png"
+              className="h-8 w-8 mx-auto opacity-20 mb-6"
             />
-            <h1 className="text-4xl font-bold text-blue-500 mb-12">
+            <h1 className="text-4xl font-bold text-blue-500 mb-6">
               Hello, there! 👋
             </h1>
             <div className="text-lg mb-4">
@@ -49,7 +71,7 @@ export const SignInTemplate = ({
           </div>
           <div className="text-gray-400 text-sm">
             Your friends at{' '}
-            <a href="">
+            <a href="https://collapp.live">
               <strong>Collapp</strong>
             </a>
           </div>
